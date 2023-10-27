bb82cfd802ae9c5b61f3d7891499f053257f504a
Bruno Apolinário <bruno.apolinario010@gmail.com>
2023-10-27 12:02:46 -0300

    Ponto 2D

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Point2D p1 = new Point2D(35, 157);
+        Point2D p2 = new Point2D(25, 49);
+        System.out.println(p1.distance(p2));
+    }
+}
